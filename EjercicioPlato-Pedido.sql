e7ded48f3e45d1f472ebdedfc7f6f34d3cb7abb4
mora avalos <moavalos@alumno.unlam.edu.ar>
2022-12-05 18:43:52 -0300

    borrando cosas q no sirven y agregando nuevos ejercicios

---
+++ EjercicioPlato-Pedido.sql
@@ -0,0 +1,145 @@
+CREATE TABLE Localidad(
+id_localidad INT PRIMARY KEY AUTO_INCREMENT,
+descripcion VARCHAR(50) NOT NULL);
+
+CREATE TABLE Plato (
+id_plato INT PRIMARY KEY AUTO_INCREMENT,
+descripcion VARCHAR (30) NOT NULL,
+precio DOUBLE NOT NULL);
+
+CREATE TABLE Cliente(
+id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+nombre VARCHAR (20) NOT NULL,
+apellido VARCHAR (20) NOT NULL,
+calle VARCHAR (30) NOT NULL,
+nro INT NOT NULL,
+id_localidad INT NOT NULL,
+FOREIGN KEY (id_localidad) REFERENCES Localidad(id_localidad));
+
+CREATE TABLE Pedido_Encabezado(
+id_pedido INT PRIMARY KEY AUTO_INCREMENT,
+id_cliente INT NOT NULL,
+fecha DATE NOT NULL,
+FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente));
+
+CREATE TABLE Pedido_Detalle(
+id_detalle INT PRIMARY KEY AUTO_INCREMENT,
+id_pedido INT NOT NULL,
+id_plato INT NOT NULL,
+cantidad INT NOT NULL,
+FOREIGN KEY(id_pedido) REFERENCES Pedido_Encabezado(id_pedido),
+FOREIGN KEY(id_plato) REFERENCES Plato(id_plato));
+
+INSERT INTO Localidad (descripcion) VALUES
+('Ramos Mejía'),
+('San Justo'),
+('Liniers'),
+('Moron');
+
+INSERT INTO Plato (descripcion, precio) VALUES
+('Empanada', 150.0),
+('Milanesa', 600.0),
+('Pizza', 1200.0),
+('Spaghetti', 800.0),
+('Hamburguesa', 700.0);
+
+INSERT INTO Cliente (nombre, apellido, calle, nro, id_localidad) VALUES
+('Carlos', 'Monzon', 'Tatacua', 1832, 3),
+('Ricardo', 'Echeverria', 'Cuba', 1743, 2),
+('Alberto', 'Estebanez', 'Castelli', 983, 1),
+('Jose', 'Bogarin', 'Av Mayo', 732, 1),
+('Maria', 'Elena', 'Arieta', 1582, 2);
+
+
+INSERT INTO Cliente (nombre, apellido, calle, nro, id_localidad) VALUES
+('Aina', 'Monzon', 'Tatacua', 1832, 3);
+
+INSERT INTO Pedido_Encabezado (id_cliente, fecha) VALUES
+(1, '2022-01-01'),
+(3, '2022-02-23'),
+(5, '2022-04-09'),
+(2, '2022-05-14'),
+(1, '2022-03-13'),
+(4, '2022-01-10');
+
+INSERT INTO Pedido_Detalle (id_pedido, id_plato, cantidad) VALUES
+(1, 4, 2),
+(1, 1, 6),
+(2, 5, 4),
+(3, 4, 2),
+(4, 2, 2),
+(5, 1, 12),
+(6, 2, 4),
+(6, 3, 2);
+
+INSERT INTO Pedido_Detalle (id_pedido, id_plato, cantidad) VALUES
+(1, 1, 6),
+(1, 2, 4),
+(1, 3, 3),
+(1, 5, 1);
+
+/*2-Obtener los datos de todos los clientes, ordenados por Localidad, Nombre y Apellido*/
+/*select c.*
+from Cliente c join Localidad l on c.id_localidad = l.id_localidad
+order by l.id_localidad, c.nombre, c.apellido*/
+
+/*3-Informar: número de Pedido, Cantidad de Platos Distintos, Cantidad de unidades total, Importe total del pedido*/
+/*select pd.id_pedido, count(pd.id_detalle) as 'cantidad de platos', sum(pd.cantidad) as 'cantidad unidades', sum(p.precio*pd.cantidad) as 'importe total'
+from Pedido_Detalle pd join Plato p on pd.id_plato = p.id_plato
+group by pd.id_pedido*/
+
+/*4-Mostrar un detalle de los clientes que han realizado pedidos en el mes de Enero y no realizaron ningún pedido en el mes de marzo*/
+/*select c.*
+from Cliente c
+where exists (select 1
+            from Pedido_Encabezado pe
+            where c.id_cliente = pe.id_cliente
+            and Month(pe.fecha) = 1)
+and not exists (select 1
+                from Pedido_Encabezado pe
+                where c.id_cliente = pe.id_cliente
+                and Month(pe.fecha) = 3)*/
+
+/*5 Informar el nombre del plato mas barato de la cartaSELECT * */
+/*select *
+from Plato
+group by id_plato, descripcion
+having min(precio)*/
+
+/*OTRA FORMA
+select *
+from plato p
+where precio = (select min (p2.precio)
+                from plato p2)*/
+
+/*6 informar los datos completos de los clientes, la fecha de última compra y el total gastado. Deben informarse la totalidad de los clientes existentes.*/
+/*select c.*, max(pe.fecha) as 'fecha ultima compra', sum(precio * pd.cantidad) as 'total gastado'
+from Cliente c join Pedido_Encabezado pe on c.id_cliente = pe.id_cliente 
+                join Pedido_Detalle pd on pe.id_pedido = pd.id_pedido
+                join Plato p on pd.id_plato = p.id_plato
+group by c.id_cliente7*/
+
+/*7 Informar los platos que han sido comprados por mas de un cliente*/
+/*select p.*
+from Plato p 
+where exists ( select 1
+            from Pedido_Detalle pd join Pedido_Encabezado pe on pd.id_pedido = pe.id_pedido
+            where pd.id_plato = p.id_plato
+            group by pd.id_plato
+            having count(distinct pe.id_cliente) > 1)*/
+
+/*8 Mostrar los clientes que han pedido todos los platos del menú*/
+/*select c.*
+from Cliente c
+join Pedido_Encabezado pe on pe.id_cliente = c.id_cliente
+join Pedido_Detalle pd on pd.id_pedido = pe.id_pedido
+group by c.id_cliente
+having count(distinct pd.id_plato) = (select count(*)
+                                        from Plato p)*/
+
+/*9-Informar la descripción y precio de los platos que no han sido comprados por ningún cliente.*/
+/*select p.descripcion, p.precio
+from Plato p
+where not exists (select 1
+                from Pedido_Detalle pd
+                where pd.id_plato = p.id_plato)*/
--- parcialtn-ejercicios.sql
+++ parcialtn-ejercicios.sql
@@ -1,81 +1,168 @@
-USE parcialtn;
+
+CREATE TABLE Localidad(
+	cod_localidad CHAR(5) PRIMARY KEY NOT NULL,
+    descripcion VARCHAR(40) NOT NULL
+);
+CREATE TABLE Cliente(
+	id_cliente CHAR(5) PRIMARY KEY NOT NULL,
+    nombre VARCHAR(40) NOT NULL, 
+    fecha_nac DATE NOT NULL,
+    cod_localidad CHAR(5) NOT NULL,
+    FOREIGN KEY (cod_localidad) REFERENCES Localidad(cod_localidad)
+);
+CREATE TABLE Empleado(
+	nro_empleado int primary key,
+	cuil INT not NULL,
+    nombre VARCHAR(40) NOT NULL,
+    sueldo DECIMAL NOT NULL,
+    cod_localidad CHAR(5) NOT NULL,
+    FOREIGN KEY (cod_localidad) REFERENCES Localidad(cod_localidad)
+);
+CREATE TABLE Reparacion(
+	nro_reparacion INT PRIMARY KEY NOT NULL,
+    fecha DATE NOT NULL,
+    importe DECIMAL NOT NULL,
+    id_cliente CHAR(5) NOT NULL,
+    nro_empleado INT NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
+    FOREIGN KEY (nro_empleado) REFERENCES Empleado(nro_empleado)
+);
+CREATE TABLE Proveedor(
+    nro_proveedor int primary key not null,
+	cuit INT NOT NULL,
+    razon_social VARCHAR(40) NOT NULL,
+    telefono INT NOT NULL
+);
+CREATE TABLE Insumo(
+	nro_insumo INT PRIMARY KEY NOT NULL,
+    descripcion VARCHAR(40) NOT NULL,
+    costo DECIMAL NOT NULL,
+    nro_proveedor INT NOT NULL,
+    FOREIGN KEY (nro_proveedor) REFERENCES Proveedor(nro_proveedor)
+);
+CREATE TABLE Reparacion_Insumo(
+	nro_reparacion INT NOT NULL,
+    nro_insumo INT NOT NULL,
+    CONSTRAINT pk_rep_ins PRIMARY KEY (nro_reparacion, nro_insumo),
+    FOREIGN KEY (nro_reparacion) REFERENCES Reparacion(nro_reparacion),
+    FOREIGN KEY (nro_insumo) REFERENCES Insumo(nro_insumo)
+);
+
+insert into Localidad(cod_localidad, descripcion) values 
+(01, 'Merlo'),
+(02, 'Libertad'),
+(03, 'La Plata');
+
+insert into Cliente(id_cliente, nombre, fecha_nac, cod_localidad) values
+(50, 'Juan Perez', '2022-10-26', 01),
+(51, 'Marta Garcia', '1988-04-21', 02),
+(52, 'Marcelo Polino', '1981-03-15', 03);
+
+insert into Empleado(nro_empleado, cuil, nombre, sueldo, cod_localidad) values
+(60, 27916389, 'Pepe', 140000, 01),
+(61, 25753578, 'Juan', 90000, 02),
+(62, 56017597, 'Monica', 100000, 03);
+
+insert into Reparacion(nro_reparacion, fecha, importe, id_cliente, nro_empleado) values
+(30, '2022-11-23', 900.00, 50, 60),
+(31, '2014-05-15', 1500.00, 51, 61),
+(32, '2022-12-01', 2000.00, 52, 62);
+
+insert into Proveedor(nro_proveedor, cuit, razon_social, telefono) values
+(20, 23547891, 'no se', 1122528877),
+(21, 87120976, 'no se 2', 1187628762),
+(22, 08657576, 'no se 3', 1173616280);
+
+insert into Insumo(nro_insumo, descripcion, costo, nro_proveedor) values
+(90, 'jdskal', 500.00, 20),
+(91, 'jdsalk', 1000.00, 21),
+(92, 'hfnbkdsa', 50000.00, 22);
+
+insert into Reparacion_Insumo(nro_reparacion, nro_insumo) values
+(30, 90),
+(31, 91),
+(32, 92);
+
+
 /*1. Agregar un nuevo cliente de nombre “Juan Perez”, id 15,
 fecha de nacimiento 21 de mayo de 1956 y código de
-localidad 34
+localidad 34*/
 
-	INSERT INTO Cliente(id, nombre, fecha_nac, cod_loc) VALUES(15, 'Juan Perez', '1956-05-21', 34);*/
+insert into Cliente (id_cliente, nombre, fecha_nac, cod_localidad) values (15, 'Juan Perez', '1956-03-21', 34);
 
 /*2. Indicar el nombre y sueldo de aquellos empleados que NO hayan realizado reparación alguna en todo el año
 2014.*/
+/*select e.nombre, e.sueldo
+from Empleado e
+where not exists (select 1
+                    from Reparacion re
+                    where re.nro_empleado = e.nro_empleado
+                    and Year(re.fecha) = 2014);*/
 
-	SELECT e.nombre, e.sueldo
-	FROM Empleado e 
-    WHERE NOT EXISTS (
-						SELECT 1
-                        FROM reparacion r 
-                        WHERE fecha BETWEEN '2014-01-01' AND '2015-01-01'
-                        AND r.cuil_empleado = e.cuil);
-                        
+
 /*3. Listar el nombre de aquellos clientes que hayan
 efectuado más de 5 reparaciones superiores a $1000.*/
-	SELECT clie.nombre
-    FROM cliente clie
-    WHERE clie.id IN (
-						SELECT rep.id_cliente
-                        FROM reparacion rep
-                        WHERE rep.importe > 1000
-                        GROUP BY rep.id_cliente, rep.importe
-                        HAVING COUNT(*) > 5);
-                        
+/*select c.nombre
+from Cliente c
+where exists (select 1
+            from Reparacion re
+            where c.id_cliente = re.id_cliente
+            and re.importe = 1000
+            group by re.nro_reparacion, c.nombre
+            having count(*) > 5)*/
+
+
 /*4. Listar las reparaciones realizadas en el año 2012. Por
 cada una de ellas mostrar: fecha, importe, nombre de
 cliente y descripción de su localidad y finalmente el
 nombre del empleado que realizó la reparación. Ordenar
 los resultados por fecha (las más recientes primero).*/
 
-	SELECT rep.fecha, rep.importe, clie.nombre, loc.descripcion, emp.nombre
-    FROM Reparacion rep JOIN Cliente clie ON rep.id_cliente = clie.id
-						JOIN Empleado emp ON emp.cuil = rep.cuil_empleado
-                        JOIN Localidad loc ON emp.cod_loc = loc.cod
-	WHERE rep.fecha BETWEEN '2012-01-01' AND '2013-01-01'
-    ORDER BY rep.fecha DESC;
+/*select re.fecha, re.importe, c.nombre, l.descripcion, e.nombre
+from Reparacion re
+join Cliente c on re.id_cliente = c.id_cliente
+join Localidad l on c.cod_localidad = l.cod_localidad
+join Empleado e on l.cod_localidad = e.cod_localidad
+where Year(re.fecha) = 2012
+order by re.fecha desc*/
+
 
 /*5. Modificar los empleados que comienzan con la letra “A”
 de manera que tengan el mismo sueldo del empleado
-de CUIL 123456789
-	
-    UPDATE Empleado e
-    SET e.sueldo = (
-					SELECT emp.sueldo
-					FROM Empleado emp
-                    WHERE emp.cuil = '123456789')
-	WHERE e.nombre LIKE '%A';*/
+de CUIL 123456789*/
 
-/*Listar la descripción de aquellos insumos que se han
+/*UPDATE Empleado e
+SET e.sueldo = ( SELECT emp.sueldo
+				FROM Empleado emp
+                WHERE emp.cuil = '123456789')
+WHERE e.nombre LIKE '%A';*/
+
+
+/*6. Listar la descripción de aquellos insumos que se han
 utilizado en todas las reparaciones.*/
 
-	SELECT ins.descripcion
-    FROM Insumo ins
-    WHERE NOT EXISTS ( 
-					SELECT 1
-                    FROM Reparacion rep
-                    WHERE NOT EXISTS(
-									SELECT 1
-                                    FROM Reparacion_Insumo ri
-                                    WHERE ri.nro_reparacion = rep.nro
-                                    AND ri.nro_reparacion = ins.nro));
-                                    
+/*select i.descripcion
+from Insumo i
+where not exists (select 1
+                from Reparacion re
+                where not exists (select 1
+                                    from Reparacion_Insumo reIn
+                                    where reIn.nro_insumo = i.nro_insumo
+                                    and reIn.nro_reparacion = re.nro_reparacion))*/
+
+
 /*7. Indicar el sueldo máximo de los empleados por cada
 localidad (descripción).*/
 
-	SELECT MAX(emp.sueldo), loc.descripcion
-    FROM empleado emp JOIN localidad loc ON emp.cod_loc = loc.cod
-    GROUP BY loc.cod, loc.descripcion;
+/*select max(e.sueldo)
+from Empleado e join Localidad l on e.cod_localidad = l.cod_localidad
+group by l.descripcion*/
+
     
-/*Eliminar a todas las reparaciones que se hayan efectuado
-con un empleado que viva en “San Justo”
-	
-    DELETE FROM Reparacion rep
+/*8. Eliminar a todas las reparaciones que se hayan efectuado
+con un empleado que viva en “San Justo”*/
+
+/*DELETE FROM Reparacion rep
     WHERE NOT EXISTS (
 						SELECT 1
                         FROM Localidad loc
@@ -83,18 +170,19 @@ con un empleado que viva en “San Justo”
 											SELECT 1 
                                             FROM Empleado emp
                                             WHERE loc.descripcion = 'San Justo'
-                                            AND emp.cuil = rep.cuil_empleado
-                                            AND emp.cod_loc = loc.cod));*/
-									
-/*Agregar un campo nuevo a la tabla Proveedor, que
+                                            AND emp.nro_empleado = rep.nro_empleado
+                                            AND emp.cod_localidad = loc.cod_localidad));*/
+	
+
+/*9. Agregar un campo nuevo a la tabla Proveedor, que
 indique la localidad donde el mismo vive, utilizando el
 tipo de dato y las restricciones de integridad que
 correspondan.*/
 
-	ALTER TABLE Proveedor
-    ADD COLUMN cod_loc SMALLINT UNSIGNED;
+ALTER TABLE Proveedor
+    ADD COLUMN cod_localidad SMALLINT UNSIGNED;
     ALTER TABLE Proveedor
-    ADD FOREIGN KEY(cod_loc) REFERENCES Localidad(cod);
+    ADD FOREIGN KEY(cod_localidad) REFERENCES Localidad(cod_localidad);
                                             
 
 
